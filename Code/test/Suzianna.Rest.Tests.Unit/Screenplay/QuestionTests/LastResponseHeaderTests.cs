ddbd4d0b010eb9951e485d6d3d7a346a250dde40
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2019-05-08 01:00:09 +0430

    assertion on LastResponseHeaderTests fixed

--- Code/test/Suzianna.Rest.Tests.Unit/Screenplay/QuestionTests/LastResponseHeaderTests.cs
+++ Code/test/Suzianna.Rest.Tests.Unit/Screenplay/QuestionTests/LastResponseHeaderTests.cs
@@ -1,11 +1,8 @@
-﻿using System.Linq;
-using System.Net.Http.Headers;
-using System.Net.Mime;
+﻿using System.Collections.Generic;
 using NFluent;
 using Suzianna.Core.Screenplay.Questions;
 using Suzianna.Rest.Screenplay.Interactions;
 using Suzianna.Rest.Screenplay.Questions;
-using Suzianna.Rest.Tests.Unit.TestDoubles;
 using Suzianna.Rest.Tests.Unit.TestUtils;
 using Xunit;
 
@@ -19,25 +16,23 @@ namespace Suzianna.Rest.Tests.Unit.Screenplay.QuestionTests
         {
             var locationHeaderValue = "http://localhost:5000/api/resource/1";
             var originValue = "*";
-            this.SetupResponse(new HttpResponseBuilder()
+            SetupResponse(new HttpResponseBuilder()
                 .WithHeader(HttpHeaders.Location, locationHeaderValue)
                 .WithHeader(HttpHeaders.AccessControlAllowOrigin, originValue)
                 .Build());
 
             Actor.AttemptsTo(Get.ResourceAt("api/resource"));
 
-            //TODO: check to find a better way
             Actor.Should(See.That(LastResponse.Headers()))
-                .HasElementThatMatches(a => a.Key == HttpHeaders.Location && a.Value.First() == locationHeaderValue)
-                .And
-                .HasElementThatMatches(a => a.Key == HttpHeaders.AccessControlAllowOrigin && a.Value.First() == originValue);
+                .ContainsPair(HttpHeaders.Location, new List<string> {locationHeaderValue})
+                .And.ContainsPair(HttpHeaders.AccessControlAllowOrigin, new List<string> {originValue});
         }
 
         [Fact]
         public void should_return_last_http_headers_by_key()
         {
             var locationHeaderValue = "http://localhost:5000/api/resource/1";
-            this.SetupResponse(new HttpResponseBuilder().WithHeader(HttpHeaders.Location, locationHeaderValue).Build());
+            SetupResponse(new HttpResponseBuilder().WithHeader(HttpHeaders.Location, locationHeaderValue).Build());
 
             Actor.AttemptsTo(Post.DataAsJson(new { }).To("api/resource"));
 
