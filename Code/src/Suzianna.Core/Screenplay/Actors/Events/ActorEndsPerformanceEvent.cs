447da939173f820b82e5a629f787d924f3d6a2f5
Hadi Ahmadi <ahmadi.hadi@outlook.com>
2019-09-03 15:19:38 +0430

    summaries on ActorPerformanceEvents

--- Code/src/Suzianna.Core/Screenplay/Actors/Events/ActorEndsPerformanceEvent.cs
+++ Code/src/Suzianna.Core/Screenplay/Actors/Events/ActorEndsPerformanceEvent.cs
@@ -2,13 +2,23 @@
 
 namespace Suzianna.Core.Screenplay.Actors.Events
 {
+    /// <summary>
+    /// Event that indicates the actor has performed a <see cref="IPerformable"/>.
+    /// </summary>
     public class ActorEndsPerformanceEvent : ISelfDescriptiveEvent
     {
+        /// <summary>
+        /// Creates an instance of the event
+        /// </summary>
+        /// <param name="actorName">Name of the actor</param>
         public ActorEndsPerformanceEvent(string actorName)
         {
             ActorName = actorName;
         }
 
+        /// <summary>
+        ///     Name of the actor
+        /// </summary>
         public string ActorName { get; private set; }
         public string Describe()
         {
